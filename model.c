0eedaed329e72a2bba472a13564b1d4606dfca56
Oliver Kwun-Morfitt <oliverkwunmorfitt@gmail.com>
2026-03-08 15:56:03 -0400

    (feat): finished logistic regression logic, learned about function scoping and function pointers

--- model.c
+++ model.c
@@ -26,3 +26,28 @@ static float logistic_loss(const float *w, const float *X, const int *y, int m, 
     return -1.0f/m * sum;
 }
 
+static void logistic_gradient(float *grad, const float *w, const float *X, const int *y, int m, int n) {
+    for (int j = 0; j < n; j++) {
+        grad[j] = 0.0f;
+    }
+
+    for (int i = 0; i < m; i++) {
+        float p_i = logistic_predict(w, X + i*n, n);
+        float error = p_i - y[i];
+        for (int j = 0; j < n; j++) {
+            grad[j] += error * X[i * n + j];
+        }
+    }
+
+    for (int j = 0; j < n; j++) {
+        grad[j] /= m;
+    }
+}
+
+Model logistic_regression_model(void) {
+    return (Model){
+        .predict = logistic_predict,
+        .loss = logistic_loss,
+        .gradient = logistic_gradient
+    };
+}
