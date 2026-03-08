294aa29ff79537dc80153c9bc0a63b797892c9bf
Oliver Kwun-Morfitt <oliverkwunmorfitt@gmail.com>
2026-03-08 15:45:28 -0400

    (feat): added logistic regression helpers

---
+++ model.c
@@ -0,0 +1,28 @@
+#include "model.h"
+#include <math.h>
+
+static float sigmoid(float z) {
+    return 1 / (1 + exp(-z));
+}
+
+static float dot(const float *a, const float *b, int n) {
+    float result = 0.0f;
+    for (int i = 0; i < n; i++) {
+        result += a[i] * b[i];
+    }
+    return result;
+}
+
+static float logistic_predict(const float *w, const float *x, int n) {
+    return sigmoid(dot(w, x, n));
+}
+
+static float logistic_loss(const float *w, const float *X, const int *y, int m, int n) {
+    float sum = 0.0f;
+    for (int i = 0; i < m; i++){
+        float p_i = logistic_predict(w, X + i * n, n);
+        sum += y[i] * log(p_i) + (1-y[i]) * log(1-p_i);
+    }
+    return -1.0f/m * sum;
+}
+
